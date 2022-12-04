269d4cdb0a0839bd7d22a5302f28be1f52fd83f9
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-04 13:30:02 -0500

    Created server.js file and routes for application. Began to build out routed for application.

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+//importing external module 
 const express = require('express');
-
+// app use express
 const app = express();
-const PORT = 3001;
+//creating port 
+const PORT = process.env.PORT || 3001;
+
+//using static assets through express middleweare 
 app.use(express.static('public'));
-
-
 // Middleware for the parsing of JSON data
 app.use(express.json());
-
 // Middleware for parsing of URL encoded data
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+//routes to route files
+require('./routes/api-routes')(app);
+
+//app listener which starts with the server
+app.listen(PORT, ()=>{
+    console.log(`
+    =============================================
+   
+    Server available at localhost${PORT}
+    
+    =============================================`);
+
+});
